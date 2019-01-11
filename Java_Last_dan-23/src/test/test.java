f7cbcab9a9393b4c4a0b8e9aa70f734c4f86cb7b
mmjoj <mmovo2@gmail.com>
2019-01-11 16:44:07 +0900

    2019-01-11 16:44:07.00

--- Java_Last_dan-23/src/test/test.java
+++ Java_Last_dan-23/src/test/test.java
@@ -17,9 +17,11 @@ public class test {
         while (true) {
             Scanner scanner = new Scanner(System.in); // 입력
             int why = 0;
-            dash();
+            String namewhy = null;
+            int aaa = 15;
+            System.out.printf("----------------------------------\n");
             System.out.println("<<원하시는 번호를 선택하세요>>");
-            dash();
+            System.out.printf("----------------------------------\n");
             System.out.println("1.과일이름입력");
             System.out.println("2.과일이름출력");
             System.out.println("3.과일이름검색");
@@ -34,264 +36,173 @@ public class test {
                 why = 9;
             }
             switch (why) {
-                case 1:
-                    input();
+                case 1: // 1.과일이름입력
+                    if (NOW >= MAX) {
+                        try {
+                            throw new Exception();
+                        } catch (Exception e) {
+                            System.out.println("현재 " + MAX + "개를 모두 입력했습니다. 더 이상 입력할 수 없습니다.");
+                        }
+                    } else {
+                        System.out.printf("----------------------------------\n");
+                        while (aaa == 15) {
+                            System.out.printf("과일 이름 입력 : ");
+                            namewhy = scanner.next();
+
+                            int returngap = 0;
+                            for (int i = 0; i < NOW; i++) {
+                                if (namewhy.equals(namelist.get(i).toString())) {
+                                    returngap = 998;
+                                }
+                            }
+                            if (returngap == 0) {
+                                aaa = 15;
+                            } else {
+                                aaa = returngap;
+                            }
+
+                            if (namewhy.contains("Hong")) {
+                                aaa = 999;
+                            }
+                            if (aaa == 15) {
+                                namelist.add(namewhy);
+                                aaa = -1;
+                            } else {
+                                if (aaa == 999) {
+                                    System.out.printf("Hong가 들어간 이름은 사용할 수 없습니다. 다른 이름을 입력해주세요.\n");
+                                } else {
+                                    System.out.printf("이미 있는 이름입니다. 다른 이름을 입력해주세요.\n");
+                                }
+                                aaa = 15;
+                            }
+                        }
+                        NOW = NOW + 1;
+                        break;
+                    }
+                case 2: // 2.과일이름출력
+                    System.out.printf("--------------------------------------------\n");
+                    if (NOW == 0) {
+
+                    } else {
+                        for (int i = 0; i < NOW; i++) {
+                            System.out.println(namelist.get(i));
+                        }
+                    }
+                    System.out.printf("--------------------------------------------\n");
                     break;
-                case 2:
-                    output();
+                case 3: // 3.과일이름검색
+                    System.out.printf("----------------------------------\n");
+                    System.out.print("과일 이름 입력하시오 : ");
+                    namewhy = scanner.next();
+
+                    int returngap = 111;
+                    for (int i = 0; i < NOW; i++) {
+                        if (namewhy.equals(namelist.get(i).toString())) {
+                            System.out.printf("--------------------------------------------\n");
+                            System.out.println(namelist.get(i));
+                            System.out.printf("--------------------------------------------\n");
+                            returngap = 0;
+                        }
+                    }
+                    if (returngap == 111) {
+                        System.out.printf("--------------------------------------------\n");
+                        System.out.println("입력한 과일 이름의 데이터가 없습니다.");
+                        System.out.printf("--------------------------------------------\n");
+                        returngap= 0;
+                    } 
                     break;
-                case 3:
-                    search();
+                case 4: // 4.과일이름수정
+                    System.out.printf("----------------------------------\n");
+                    System.out.print("과일 이름 입력하시오 : ");
+                    namewhy = scanner.next();
+                    int tmpmp=0;
+                    returngap = 0;
+                    for (int i = 0; i < NOW; i++) {
+                        if (namewhy.equals(namelist.get(i).toString())) {
+                            returngap = i;
+                            tmpmp=1;
+                        }
+                    }
+                    if (tmpmp == 1) {
+                        aaa = returngap;
+                    } else {
+                        aaa = 15;
+                    }
+                    if (aaa == 15) {
+                        System.out.printf("--------------------------------------------\n");
+                        System.out.println("입력한 과일 이름의 데이터가 없습니다.");
+                        System.out.printf("--------------------------------------------\n");
+                    } else {
+                        System.out.printf("--------------------------------------------\n");
+                        System.out.print("과일 이름(현재 " + namelist.get(aaa) + ") 수정 : ");
+                        namelist.set(aaa, scanner.next());
+                        System.out.printf("--------------------------------------------\n");
+                        System.out.println(namelist.get(aaa));
+                        System.out.printf("--------------------------------------------\n");
+                    }
                     break;
-                case 4:
-                    modified();
+                case 5: // 5.과일이름삭제
+                    System.out.printf("----------------------------------\n");
+                    System.out.print("과일 이름 입력하시오 : ");
+                    namewhy = scanner.next();
+                    for (int i = 0; i < NOW; i++) {
+                        if (namewhy.equals(namelist.get(i).toString())) {
+                            aaa = i;
+                        }
+                    }
+                    if (aaa == 15) {
+                        System.out.printf("--------------------------------------------\n");
+                        System.out.println("입력한 과일 이름의 데이터가 없습니다.");
+                        System.out.printf("--------------------------------------------\n");
+                    } else {
+                        namelist.remove(aaa);
+                        NOW = NOW - 1;
+                        System.out.printf("--------------------------------------------\n");
+                        if (NOW == 0) {
+
+                        } else {
+                            for (int i = 0; i < NOW; i++) {
+                                System.out.println(namelist.get(i));
+                            }
+                        }
+                        System.out.printf("--------------------------------------------\n");
+                    }
                     break;
-                case 5:
-                    delete();
+                case 6: // 6.과일이름정렬
+                    boolean status_tmp = true;
+                    while (status_tmp == true) {
+                        System.out.printf("오름차순=='1', 내림차순=='2' 입력 : ");
+                        String tmp123 = scanner.next();
+
+                        if (tmp123.equals("1")) {
+                            // 오름차순
+                            Collections.sort(namelist, new Comparator<String>() {
+                                public int compare(String obj1, String obj2) {
+                                    return obj1.compareToIgnoreCase(obj2);
+                                }
+                            });
+                            status_tmp = false;
+                        } else if (tmp123.equals("2")) {
+                            // 내림차순
+                            Collections.sort(namelist, new Comparator<String>() {
+                                public int compare(String obj1, String obj2) {
+                                    return obj1.compareToIgnoreCase(obj2);
+                                }
+                            });
+                            Collections.reverse(namelist);
+                            status_tmp = false;
+                        } else {
+                            status_tmp = true;
+                        }
+                    }
                     break;
-                case 6:
-                    align1();
-                    /*
-				align1();
-                                align2();
-                                align3();*/
-                    break;
-                case 7:
+                case 7: // 7.종료
                     System.out.println("프로그램을 종료합니다.");
                     System.exit(0);
                     break;
                 default:
                     System.out.println("올바른 값을 적어주세요.");
             }
-        }
-    }
-
-    static int search(int NOW, ArrayList<String> arraygap, String name) {
-        int returngap = 0;
-        for (int i = 0; i < NOW; i++) {
-            if (name.equals(arraygap.get(i).toString())) {
-                returngap = i;
-                return returngap;
-            }
-        }
-        return 15;
-    }
-
-    static void dash() {
-        System.out.printf("----------------------------------\n");
-    }
-
-    static void dash(int a) {
-        for (int i = 0; i < a; i++) {
-            System.out.printf("-");
-        }
-        System.out.printf("\n");
-    }
-
-    static void delete() {
-        Scanner scanner = new Scanner(System.in); // 입력
-
-        String namewhy = null;
-        int aaa = 15;
-
-        dash();
-        System.out.print("이름 입력하시오 : ");
-        namewhy = scanner.next();
-
-        for (int i = 0; i < NOW; i++) {
-            if (namewhy.equals(namelist.get(i).toString())) {
-                aaa = i;
-            }
-        }
-
-        if (aaa == 15) {
-            dash(45);
-            System.out.println("입력한 이름의 데이터가 없습니다.");
-            dash(45);
-        } else {
-            for (int i = 0; i < NOW; i++) {
-
-            }
-
-            namelist.remove(aaa);
-            NOW = NOW - 1;
-
-            dash(45);
-            if (NOW == 0) {
-
-            } else {
-                for (int i = 0; i < NOW; i++) {
-                    System.out.println(namelist.get(i));
-                }
-            }
-            dash(45);
-        }
-    }
-
-    static void input() {
-        Scanner scanner = new Scanner(System.in); // 입력		
-
-        if (NOW >= MAX) {
-            try {
-                throw new Exception();
-            } catch (Exception e) {
-                System.out.println("현재 " + MAX + "개를 모두 입력했습니다. 더 이상 입력할 수 없습니다.");
-            }
-        } else {
-            dash();
-            String namewhy = null;
-            int aaa = 15;
-
-            while (aaa == 15) {
-                System.out.printf("과일 이름 입력 : ");
-                namewhy = scanner.next();
-
-                aaa = search(NOW, namelist, namewhy);
-
-                if (namewhy.contains("Hong")) {
-                    aaa = 999;
-                }
-
-                if (aaa == 15) {
-                    namelist.add(namewhy);
-                    aaa = -1;
-                } else {
-                    if (aaa == 999) {
-                        System.out.printf("Hong가 들어간 이름은 사용할 수 없습니다. 다른 이름을 입력해주세요.\n");
-                    } else {
-                        System.out.printf("이미 있는 이름입니다. 다른 이름을 입력해주세요.\n");
-                    }
-
-                    aaa = 15;
-                }
-            }
-
-            NOW = NOW + 1;
-        }
-    }
-
-    static void align1() {
-        Scanner scanner = new Scanner(System.in); // 입력	
-        boolean status_tmp = true;
-
-        while (status_tmp == true) {
-            System.out.printf("오름차순=='1', 내림차순=='2' 입력 : ");
-            String tmp123 = scanner.next();
-
-            if (tmp123.equals("1")) {
-                // 오름차순
-                Collections.sort(namelist, new Comparator<String>() {
-                    public int compare(String obj1, String obj2) {
-                        return obj1.compareToIgnoreCase(obj2);
-                    }
-                });
-                status_tmp = false;
-            } else if (tmp123.equals("2")) {
-                // 오름차순
-                Collections.sort(namelist, new Comparator<String>() {
-                    public int compare(String obj1, String obj2) {
-                        return obj1.compareToIgnoreCase(obj2);
-                    }
-                });
-                Collections.reverse(namelist);
-                status_tmp = false;
-            } else {
-                status_tmp = true;
-            }
-        }
-    }
-
-    static void output() {
-        dash(45);
-        /*
-		align1(numlist);
-		align1(korlist);
-		align1(englist);
-		align1(matlist);
-		align1(ranklist);
-		align2(namelist);
-		align3(avglist);
-		align1(sumlist);*/
-
-        if (NOW == 0) {
-
-        } else {
-            for (int i = 0; i < NOW; i++) {
-                System.out.println(namelist.get(i));
-            }
-        }
-        dash(45);
-    }
-
-    static void modified() {
-        Scanner scanner = new Scanner(System.in); // 입력
-
-        String namewhy = null;
-        int aaa = 15;
-
-        dash();
-        System.out.print("과일 이름 입력하시오 : ");
-        namewhy = scanner.next();
-
-        aaa = search(NOW, namelist, namewhy);
-        /*
-		for(int i=0;i<NOW;i++){
-			if(namewhy.equals(namelist.get(i).toString())){
-				aaa=i;
-			}
-		}*/
-
-        if (aaa == 15) {
-            dash(45);
-            System.out.println("입력한 과일 이름의 데이터가 없습니다.");
-            dash(45);
-        } else {
-            dash(45);
-            System.out.print("과일이름(현재 " + namelist.get(aaa) + ")점 수정점수 : ");
-            namelist.set(aaa, scanner.next());
-
-            dash(45);
-            System.out.println(namelist.get(aaa));
-            dash(45);
-            /*
-			align1(numlist);
-			align1(korlist);
-			align1(englist);
-			align1(matlist);
-			align1(ranklist);
-			align2(namelist);
-			align3(avglist);
-			align1(sumlist);*/
-        }
-    }
-
-    static void search() {
-
-        Scanner scanner = new Scanner(System.in); // 입력
-
-        String namewhy = null;
-        int aaa = 15;
-
-        dash();
-        System.out.print("과일 이름 입력하시오 : ");
-        namewhy = scanner.next();
-
-        aaa = search(NOW, namelist, namewhy);
-        /*
-		for(int i=0;i<NOW;i++){
-			if(namewhy.equals(namelist.get(i).toString())){
-				aaa=i;
-			}
-		}*/
-
-        if (aaa == 15) {
-            dash(45);
-            System.out.println("입력한 과일 이름의 데이터가 없습니다.");
-            dash(45);
-        } else {
-            dash(45);
-            System.out.println(namelist.get(aaa));
-            dash(45);
         }
     }
 }
